ea82a2134593fedf4cd34734c868cbdff8f9b099
markgordon22 <markgordon97@gmail.com>
2021-05-20 20:21:11 +0000

    lk

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
+    tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
 
@@ -84,12 +84,20 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
 
     if session["user"]:
         return render_template("profile.html", username=username)
 
     return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "left"});
+    $('.collapsible').collapsible();
+    $(".tooltipped").tooltip();
 });
+
+ 
+
