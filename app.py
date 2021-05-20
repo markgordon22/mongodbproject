4315b0dc1d7a4c383fb58e8a2093d216d891fbeb
markgordon22 <markgordon97@gmail.com>
2021-05-20 15:38:36 +0000

    app.py update

--- app.py
+++ app.py
@@ -86,6 +86,10 @@ def profile(username):
         {"username": session["user"]})["username"]
     return render_template("profile.html", username=username)
 
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
